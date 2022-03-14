9ffc4226179545ab0c26f834e5180d9c7c15c8a2
Jarrett Hopkins <jarrettphopkins@Gmail.com>
2022-03-13 23:39:43 -0400

    push to git

---
+++ Assets/js/script.js
@@ -0,0 +1,46 @@
+//tells what the time/day is when page opens
+let todaysDate = moment().format('dddd, MMMM Do');
+$("#currentDay").append(todaysDate);
+
+$(".saveBtn").on("click", function () {
+    let textArea = $(this).siblings(".textarea").val();
+    let hours = $(this).parent().attr("id");
+    localStorage.setItem(hours, textArea);
+})
+
+$(document).ready(function () {
+
+    $("#9").val(localStorage.getItem("hours9"));
+    $("#10").val(localStorage.getItem("hours10"));
+    $("#11").val(localStorage.getItem("hours11"));
+    $("#12").val(localStorage.getItem("hours12"));
+    $("#13").val(localStorage.getItem("hours1"));
+    $("#14").val(localStorage.getItem("hours2"));
+    $("#15").val(localStorage.getItem("hours3"));
+    $("#16").val(localStorage.getItem("hours4"));
+    $("#17").val(localStorage.getItem("hours5"));
+})
+
+
+//functions tracks time
+let auditTime = function () {
+    //set variable what time of day it is
+    let timeNow = moment().hours();
+
+    $(".textarea").each(function () {
+        let currenthours = $(this).attr("id");
+        if (timeNow > currenthours) {
+            $(this).addClass("past");
+            $(this).removeClass("present future");
+        } else if (timeNow == currenthours) {
+            $(this).addClass("present");
+            $(this).removeClass("past future");
+        } else if (timeNow < currenthours) {
+            $(this).addClass("future");
+            $(this).removeClass("past present");
+        }
+    })
+}
+
+
+auditTime();
---
+++ README.md
@@ -0,0 +1,19 @@
+# Work Day Scheduler Starter Code
+
+[<img width="1592" alt="Screen Shot at 3/13/22" src="https://user-images.githubusercontent.com/98188411/156968699-f2a83661-fc8c-462f-8135-4c5c7a022360.JPG">](https://jhopkins387.github.io/Hopkins_Password_Generator)
+
+# Schedule Plainer
+
+## Our Purpose
+A planner that keeps track of your progress and saves what you put in.
+
+## Built Using
+CSS/HTML/JS
+
+## Visit our Website
+
+https://jhopkins387.github.io/Hopkins_Code_Quiz/
+
+# Project Completed By:
+ 
+* Jarrett Hopkins
